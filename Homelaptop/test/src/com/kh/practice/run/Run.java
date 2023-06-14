913c929f7f45309d9a727abf912bcc89906a7907
Minji0A0 <mingmingji0a0@gmail.com>
2023-06-15 01:59:37 +0900

    Update Run.java

--- Homelaptop/test/src/com/kh/practice/run/Run.java
+++ Homelaptop/test/src/com/kh/practice/run/Run.java
@@ -6,20 +6,20 @@ public class Run {
 	public static void main(String[] args) {
 
 		ArrayPractice arrayp = new ArrayPractice();
-//		arrayp.practice1();
-//		arrayp.practice2();
-//		arrayp.practice3();
-//		arrayp.practice4();
-//		arrayp.practice5();
-//		arrayp.practice6();
-//		arrayp.practice7();
-//		arrayp.practice8();
-//		arrayp.practice9();
-//		arrayp.practice10();
-//		arrayp.practice11();
-//		arrayp.practice12();
-//		arrayp.practice13();
-//		arrayp.practice14();
+		arrayp.practice1();
+		arrayp.practice2();
+		arrayp.practice3();
+		arrayp.practice4();
+		arrayp.practice5();
+		arrayp.practice6();
+		arrayp.practice7();
+		arrayp.practice8();
+		arrayp.practice9();
+		arrayp.practice10();
+		arrayp.practice11();
+		arrayp.practice12();
+		arrayp.practice13();
+		arrayp.practice14();
 		arrayp.practice15();
 		arrayp.practice16();
 
